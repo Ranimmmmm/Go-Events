be41c82c07dd5b15460a5f07c8f67cd9860b260f
ranim mouadeb <rmoaudb66@gmail.com>
2024-07-31 17:58:18 +0100

    fetch errors in create events section

--- starting-project/src/util/http.js
+++ starting-project/src/util/http.js
@@ -14,4 +14,25 @@ export default async function fetchEvents({signal , searchTerm}) {
     const { events } = await response.json();
 
     return events;
+  }  
+
+  export async function createNewEvent(eventData) {
+    const response = await fetch(`http://localhost:3000/events`, {
+      method: 'POST',
+      body: JSON.stringify(eventData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  
+    if (!response.ok) {
+      const error = new Error('An error occurred while creating the event');
+      error.code = response.status;
+      error.info = await response.json();
+      throw error;
+    }
+  
+    const { event } = await response.json();
+  
+    return event;
   }
