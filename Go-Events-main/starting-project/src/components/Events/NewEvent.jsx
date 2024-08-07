afda74855a61ea778679c5b529cdfb5c4ae4c333
ranim mouadeb <rmoaudb66@gmail.com>
2024-08-07 11:17:07 +0100

    Initial commit with Netlify configuration

---
+++ Go-Events-main/starting-project/src/components/Events/NewEvent.jsx
@@ -0,0 +1,46 @@
+import { Link, useNavigate } from 'react-router-dom';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import Modal from '../UI/Modal.jsx';
+import EventForm from './EventForm.jsx';
+import { createNewEvent } from '../../util/http.js';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
+import { queryClient } from '../../util/http.js';
+export default function NewEvent() {
+  const navigate = useNavigate();
+  const {mutate, isPending, isError, error} = useMutation({
+    mutationFn: createNewEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({querykey : ['events']});
+      navigate('/events');
+    }
+
+  });
+
+  function handleSubmit(formData) {
+    mutate({event: formData});
+
+  }
+
+  return (
+    <Modal onClose={() => navigate('../')}>
+      <EventForm onSubmit={handleSubmit}>
+        {isPending && 'Submitting .. '}
+        {!isPending &&(
+          <>
+              <Link to="../" className="button-text">
+              Cancel
+            </Link>
+            <button type="submit" className="button">
+              Create
+            </button>
+          </>
+        )}
+      
+      </EventForm>
+        
+      {isError && 
+      <ErrorBlock title="An error occurred" message={error.info?.message || "Failed to create event" }/>
+    }
+    </Modal>
+  );
+}
